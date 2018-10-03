2e8255eca2ba461a742e26f0f1ffa556c915d11d
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-10-04 00:11:18 +0200

    Añado clase de testing

--- PruebasUnitarias/UnitTest1.cs
+++ PruebasUnitarias/UnitTest1.cs
@@ -1,13 +1,64 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PostPruebasUnitarias;
 
 namespace PruebasUnitarias
 {
     [TestClass]
-    public class UnitTest1
+    public class PruebasProcesador
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSuma()
         {
+            //Definimos las variables
+            //Arrange
+            var operador1 = 10;
+            var operador2 = 11;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Sumar(operador1, operador2);
+
+            //Comparamos resultados
+            //Assert
+            var valorEsperado = 21;
+            Assert.AreEqual(valorEsperado, result);
+        }
+
+        [TestMethod]
+        public void TestDivision1()
+        {
+            //Definimos las variables
+            //Arrange
+            var dividendo = 10;
+            var divisor = 2;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Division(dividendo, divisor);
+
+            //Comparamos resultados
+            //Assert
+            var valorEsperado = 5;
+            Assert.AreEqual(valorEsperado, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))] //Excepcion esperada
+        public void TestDivisionExcepcion()
+        {
+            //Definimos las variables
+            //Arrange
+            var dividendo = 10;
+            var divisor = 0;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Division(dividendo, divisor);
+
+            //Comparamos resultados
+            //Assert
+            //En este caso, no deberiamos llegar aqui, la prueba correcta es una excepcion de DivisionEntreCero
         }
     }
 }
